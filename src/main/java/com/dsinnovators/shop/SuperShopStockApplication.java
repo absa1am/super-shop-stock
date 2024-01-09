081b503e660ffe983c9907bb4ef28696b7e51cf5
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-01-09 12:48:34 +0600

    Initial Project

---
+++ src/main/java/com/dsinnovators/shop/SuperShopStockApplication.java
@@ -0,0 +1,13 @@
+package com.dsinnovators.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperShopStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperShopStockApplication.class, args);
+	}
+
+}
