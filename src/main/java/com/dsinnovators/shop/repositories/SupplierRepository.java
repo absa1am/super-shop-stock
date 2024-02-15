9a70d47a4bc275c8a348077bbd7d6fd75ccf42cc
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-02-16 01:47:31 +0600

    Add server side pagination

--- src/main/java/com/dsinnovators/shop/repositories/SupplierRepository.java
+++ src/main/java/com/dsinnovators/shop/repositories/SupplierRepository.java
@@ -1,6 +1,8 @@
 package com.dsinnovators.shop.repositories;
 
 import com.dsinnovators.shop.entities.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,6 @@ import java.util.List;
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
 
     List<Supplier> findAllByIsDeletedIsFalse();
+    Page<Supplier> findAllByIsDeletedIsFalse(Pageable pageable);
 
 }
