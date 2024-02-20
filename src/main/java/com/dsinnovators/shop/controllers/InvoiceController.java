2271ba0c69b7e6b1bc93895b8122be95645e30b9
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-02-20 17:35:31 +0600

    Add invoice feature and fix bug

--- src/main/java/com/dsinnovators/shop/controllers/InvoiceController.java
+++ src/main/java/com/dsinnovators/shop/controllers/InvoiceController.java
@@ -1,21 +1,38 @@
 package com.dsinnovators.shop.controllers;
 
+import com.dsinnovators.shop.entities.Invoice;
+import com.dsinnovators.shop.entities.Product;
 import com.dsinnovators.shop.services.InvoiceService;
+import com.dsinnovators.shop.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class InvoiceController {
 
     private InvoiceService invoiceService;
+    private ProductService productService;
 
-    public InvoiceController(InvoiceService invoiceService) {
+    public InvoiceController(InvoiceService invoiceService, ProductService productService) {
         this.invoiceService = invoiceService;
+        this.productService = productService;
+    }
+
+    @ResponseBody
+    @GetMapping("/products")
+    public List<Product> product() {
+        return productService.getProducts();
     }
 
     @GetMapping("/invoice/create")
     public String create(Model model) {
+        model.addAttribute("invoice", new Invoice());
+        model.addAttribute("products", productService.getProducts());
+
         return "invoice/create";
     }
 
